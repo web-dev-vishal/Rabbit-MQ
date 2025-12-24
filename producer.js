50a1f7b66049523f3f94ac6f0999b3c488bed300
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-24 22:45:59 +0530

    commit

--- producer.js
+++ producer.js
@@ -2,5 +2,27 @@ const amqp = require("amqplib");
 
 
 async function sendMail() {
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel()
+        const exchange = "mail_exchange";
+        const routingKey = "send_mail"
 
+        const message = {
+            to: "vishalsanam2024@gmail.com",
+            from: "guyr07332@gmail.com",
+            subject: "Hello TP mail",
+            body: "Hello ProMan!!!"
+        }
+
+        await channel.assertExchange(exchange, "direct", {durable: false});
+        await channel.assertQueue("mail_queue", {durable: false});
+        
+
+
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+sendMail();
