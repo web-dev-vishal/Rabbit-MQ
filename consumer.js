5511960c073ee7e09985c66d50bfc7a798ed9c5e
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-24 23:25:16 +0530

    commit

--- consumer.js
+++ consumer.js
@@ -4,6 +4,14 @@ async function recvMail() {
     try {
         const connection = await amqp.connect("amqp://localhost");
         const channel = await connection.createChannel()
+
+        await channel.assertQueue("mail_queue", { durable: false });
+
+        channel.consume("mail_queue", (message) => {
+            if (!message == null) {
+                console.log("recv message", JSON.parse(message.content))
+            }
+        })
     } catch (error) {
         console.log(error)
     }
