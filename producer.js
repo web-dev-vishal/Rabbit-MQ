967b206ff23be0296b1287aa2ca80594c09c9596
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-25 14:19:43 +0530

    commit

--- producer.js
+++ producer.js
@@ -7,8 +7,8 @@ async function sendMail() {
         const channel = await connection.createChannel()
 
         const exchange = "mail_exchange";
-        const routingKeyforSubUser = "send_mail_to_subcribed_users"
-        const routingKeyforNormalUser = "send_mail_to_users"
+        const routingKeyForSubUser = "send_mail_to_subcribed_users"
+        const routingKeyForNormalUser = "send_mail_to_users"
 
         const message = {
             to: "abc@gmail.com",
@@ -18,10 +18,12 @@ async function sendMail() {
         }
 
         await channel.assertExchange(exchange, "direct", { durable: false });
+
         await channel.assertQueue("subcribed_users_mail_queue", { durable: false });
         await channel.assertQueue("mail_queue", { durable: false });
 
-        await channel.bindQueue("mail_queue", exchange, routingKey)
+        await channel.bindQueue("subscribed_usres_mail_queue", exchange, routingKeyForSubUser)
+        await channel.bindQueue("usres_mail_queue", exchange, routingKeyForNormalUser)
 
         channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)));
         console.log("Mail data was sent", message);
