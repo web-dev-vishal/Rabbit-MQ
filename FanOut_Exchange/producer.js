784b5a71a703533ee5a7061c1d278714d6e96442
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-26 15:25:40 +0530

    commit

--- FanOut_Exchange/producer.js
+++ FanOut_Exchange/producer.js
@@ -1,15 +1,18 @@
 const amqp = require("amqplib");
 
-const sendMessage = async (routingKey, message) => {
+const announceNewProduct = async (product) => {
     try {
         const connection = await amqp.connect("amqp://localhost");
         const channel = await connection.createChannel();
-        const exchange = "notification_exchange";
-        const exchangeType = "topic";
+        const exchange = "new_product_launch";
+        const exchangeType = "fanout";
 
         await channel.assertExchange(exchange, exchangeType, { durable: true });
 
+
+        
         channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)), { persistent: true });
+
         console.log("[x] Sent '%s': '%s'", routingKey, JSON.stringify(message));
         console.log(`Message was send! with routing key as ${routingKey} and content as ${JSON.stringify(message)}`);
 
