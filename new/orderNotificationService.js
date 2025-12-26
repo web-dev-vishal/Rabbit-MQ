22cf8e2d1ab7e6060ca95ad7dd1e3933990538f4
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-26 08:04:31 +0530

    commit

--- new/orderNotificationService.js
+++ new/orderNotificationService.js
@@ -11,17 +11,20 @@ const receiveMessage = async () => {
         await channel.assertExchange (exchange, "topic", {durable: true});
         await channel.assertExchange (queue, {durable: true});
 
-        console.log("[x] Sent '%s: '$s'", routingkey, JSON.stringify(message));
-        console.log(`Message was send! with routing ket as ${routingKey} and content as ${message}`);
+        console.log("Waiting for messages");
+        channel.consume(
+        queue,
+        (msg) => {
+            if (msg !== null) {
+                console.log(`[Order Notification] Msg was consumed! with routingKey`);
+                channel.ack(msg);
+            };
+        },{ noAck: false}
+        )
 
-        setTimeout(()=> {
-            connection.close();
-        }, 500);
     } catch (error) {
         console.log("Error:", error)
     }
 };
 
-sendMessage("order.placed", {orderId: 12345, status: "placed"});
-
-sendMessage("payment.processed", {paymentId: 6789, status: "proccessed"})
+receiveMessage();
