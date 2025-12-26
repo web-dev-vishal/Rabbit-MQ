9d422c3ce7cfbb3a8135f70eb958510968c67f28
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-26 08:19:20 +0530

    commit

--- new/orderNotificationService.js
+++ new/orderNotificationService.js
@@ -1,31 +1,65 @@
+// const amqp = require("amqplib");
+
+// const receiveMessage = async () => {
+//     try {
+//         const connection = await amqp.connect("amqp://localhost");
+//         const channel = await connection.createChannel()
+
+//         const exchange = "norification_exchange";
+//         const queue = "order_queue";
+
+//         await channel.assertExchange (exchange, "topic", {durable: true});
+//         await channel.assertQueue (queue, {durable: true});
+
+//         await channel.bindQueue(queue, exchange, "order.*")
+
+//         console.log("Waiting for messages");
+//         channel.consume(
+//         queue,
+//         (msg) => {
+//             if (msg !== null) {
+//                 console.log(`[Order Notification] Msg was consumed! with routingKey`);
+//                 channel.ack(msg);
+//             };
+//         },{ noAck: false}
+//         )
+
+//     } catch (error) {
+//         console.log("Error:", error)
+//     }
+// };
+
+// receiveMessage();
+
 const amqp = require("amqplib");
 
 const receiveMessage = async () => {
     try {
         const connection = await amqp.connect("amqp://localhost");
-        const channel = await connection.createChannel()
+        const channel = await connection.createChannel();
 
-        const exchange = "norification_exchange";
+        const exchange = "mail_exchange";
         const queue = "order_queue";
 
-        await channel.assertExchange (exchange, "topic", {durable: true});
-        await channel.assertQueue (queue, {durable: true});
+        await channel.assertExchange(exchange, "topic", {durable: true});
+        await channel.assertQueue(queue, {durable: true});
 
-        await channel.bindQueue(queue, exchange, "order.*")
+        await channel.bindQueue(queue, exchange, "order.*");
 
         console.log("Waiting for messages");
         channel.consume(
-        queue,
-        (msg) => {
-            if (msg !== null) {
-                console.log(`[Order Notification] Msg was consumed! with routingKey`);
-                channel.ack(msg);
-            };
-        },{ noAck: false}
-        )
+            queue,
+            (msg) => {
+                if (msg !== null) {
+                    console.log(`[Order Notification] Msg was consumed! with routingKey: ${msg.fields.routingKey} and content as ${msg.content.toString()}`);
+                    channel.ack(msg);
+                }
+            },
+            { noAck: false }
+        );
 
     } catch (error) {
-        console.log("Error:", error)
+        console.log("Error:", error);
     }
 };
 
