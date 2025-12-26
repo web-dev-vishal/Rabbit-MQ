3b86c521505184b8fbab7b63d833b042987b7213
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-26 08:19:50 +0530

    commit

--- new/producer.js
+++ new/producer.js
@@ -1,27 +1,55 @@
+// const amqp = require("amqplib");
+
+// const sendMessage = async (routingKey, message) => {
+//     try {
+//         const connection = await amqp.connect("amqp://localhost");
+//         const channel = await connection.createChannel()
+
+//         const exchange = "mail_exchange";
+//         const exchangeType = "topic";
+
+//         await channel.assertExchange (exchange, exchangeType, {durable: true});
+
+//         channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)), {persistent: true});
+//         console.log("[x] Sent '%s: '$s'", routingkey, JSON.stringify(message));
+//         console.log(`Message was send! with routing ket as ${routingKey} and content as ${message}`);
+
+//         setTimeout(()=> {
+//             connection.close();
+//     }, 500);
+//     } catch (error) {
+//         console.log("Error:", error)
+//     }
+// };
+
+// sendMessage("order.placed", {orderId: 12345, status: "placed"});
+
+// sendMessage("payment.processed", {paymentId: 6789, status: "proccessed"})
+
 const amqp = require("amqplib");
 
 const sendMessage = async (routingKey, message) => {
     try {
         const connection = await amqp.connect("amqp://localhost");
-        const channel = await connection.createChannel()
+        const channel = await connection.createChannel();
 
         const exchange = "mail_exchange";
         const exchangeType = "topic";
 
-        await channel.assertExchange (exchange, exchangeType, {durable: true});
+        await channel.assertExchange(exchange, exchangeType, {durable: true});
 
         channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)), {persistent: true});
-        console.log("[x] Sent '%s: '$s'", routingkey, JSON.stringify(message));
-        console.log(`Message was send! with routing ket as ${routingKey} and content as ${message}`);
+        console.log("[x] Sent '%s': '%s'", routingKey, JSON.stringify(message));
+        console.log(`Message was send! with routing key as ${routingKey} and content as ${JSON.stringify(message)}`);
 
-        setTimeout(()=> {
+        setTimeout(() => {
             connection.close();
-    }, 500);
+        }, 500);
     } catch (error) {
-        console.log("Error:", error)
+        console.log("Error:", error);
     }
 };
 
 sendMessage("order.placed", {orderId: 12345, status: "placed"});
 
-sendMessage("payment.processed", {paymentId: 6789, status: "proccessed"})
+sendMessage("payment.processed", {paymentId: 6789, status: "processed"});
