c8dfe2ff13fabcb7aaa452597b7f4816b83893f4
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-26 12:55:09 +0530

    commit

--- new/orderNotificationService.js
+++ new/orderNotificationService.js
@@ -1,36 +1,3 @@
-// const amqp = require("amqplib");
-
-// const receiveMessage = async () => {
-//     try {
-//         const connection = await amqp.connect("amqp://localhost");
-//         const channel = await connection.createChannel()
-
-//         const exchange = "norification_exchange";
-//         const queue = "order_queue";
-
-//         await channel.assertExchange (exchange, "topic", {durable: true});
-//         await channel.assertQueue (queue, {durable: true});
-
-//         await channel.bindQueue(queue, exchange, "order.*")
-
-//         console.log("Waiting for messages");
-//         channel.consume(
-//         queue,
-//         (msg) => {
-//             if (msg !== null) {
-//                 console.log(`[Order Notification] Msg was consumed! with routingKey`);
-//                 channel.ack(msg);
-//             };
-//         },{ noAck: false}
-//         )
-
-//     } catch (error) {
-//         console.log("Error:", error)
-//     }
-// };
-
-// receiveMessage();
-
 const amqp = require("amqplib");
 
 const receiveMessage = async () => {
