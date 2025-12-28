368b8c817791f4f71784a8a55b6d05907efea48f
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-28 08:26:13 +0530

    Header_Exchange

---
+++ Header_Exchange/LiveStreamNotifactions.js
@@ -0,0 +1,38 @@
+const amqp = require("amqplib");
+
+const consumeLiveStreamNotifications = async () => {
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        const exchange = "header_exchange";
+        const exchangeType = "headers";
+
+        await channel.assertExchange(exchange, exchangeType, { durable: true });
+
+        // await channel.assertQueue(queue, {durable: true});
+
+        const q = await channel.assertExchange("", { exclusive: true });
+        console.log("Waiting for live stream notifiactions");
+
+        await channel.bindQueue(q.queue, exchange, "", {
+            "x-watch": "all",
+        "notifiaction-type": "Live_stream",
+            "content-type": "gaming"
+        });
+
+        // console.log("Waiting for messages");
+        channel.consume(q.queue, (msg) => {
+            if (msg !== null) {
+                const message = msg.content.toString();
+                console.log(" Recevide new video notification", message);
+                // Process the notification
+                console.ack(msg)
+            }
+        });
+
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
+
+consumeLiveStreamNotifications();
