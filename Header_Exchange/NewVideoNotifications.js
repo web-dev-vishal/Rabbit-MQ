dcbd6617d6eb57b563ad4ebdd16b26911a090b20
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-28 08:15:54 +0530

    Header_Exchange

---
+++ Header_Exchange/NewVideoNotifications.js
@@ -0,0 +1,37 @@
+const amqp = require("amqplib");
+
+const receiveMessage = async () => {
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        const exchange = "header_exchange";
+        const queue = "headers";
+
+        await channel.assertExchange(exchange, "exchangeType", { durable: true });
+
+        // await channel.assertQueue(queue, {durable: true});
+
+        const q = await channel.assertExchange("", { exclusive: true });
+        console.log("Waiting for new video notifiactions");
+
+        await channel.bindQueue(queue, exchange, "", {
+            "x-watch": "all",
+            "notifiaction-type": "new_video",
+            "content-type": "video"
+        });
+
+        // console.log("Waiting for messages");
+        channel.consume(q.queue, (msg) => {
+            if (msg !== null) {
+                const message = msg.content.toString();
+                console.log(" Recevide new video notification", message);
+                console.ack(msg)
+            }
+        });
+
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
+
+receiveMessage();
