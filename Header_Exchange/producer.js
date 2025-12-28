51ad086c7a76838e4133e8f7d682f482f4e8d95a
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-28 07:43:02 +0530

    Header_Exchange

---
+++ Header_Exchange/producer.js
@@ -0,0 +1,26 @@
+const amqp = require("amqplib");
+
+const announceNewProduct = async (product) => {
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+
+        const exchange = "new_product_launch";
+        const exchangeType = "fanout";
+
+        await channel.assertExchange(exchange, exchangeType, { durable: true });
+
+        const message = JSON.stringify(product);
+
+        channel.publish(exchange, "", Buffer.from(message), { persistent: true });
+        console.log("sent => ", message);
+
+        setTimeout(() => {
+            connection.close()
+        }, 500);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
+
+announceNewProduct({ id: 123, name: "iphone 20 pro max", price: 200000});
