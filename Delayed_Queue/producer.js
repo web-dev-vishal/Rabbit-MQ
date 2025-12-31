9538143b72839997844b65d104bf85e72b2e4f9a
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-31 19:49:19 +0530

    commit

--- Delayed_Queue/producer.js
+++ Delayed_Queue/producer.js
@@ -1,2 +1,19 @@
 const amqp = require("amqplib");
 
+async function sendToDelayedQueue(batchId, orders, delay) { 
+}
+
+async function processBatchOrders (batchId, orders, delay) { 
+}
+
+function generateBatchId() {
+}
+
+function collectOrdersForBatch(){
+
+}
+async function processOrders(orders) {
+    
+}
+
+processOrders();
