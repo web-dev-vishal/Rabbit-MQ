fc31a0adacf2d3bfb150ab140addeeb362527dd2
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-31 06:59:10 +0530

    commit

--- subConsumer.js
+++ subConsumer.js
@@ -3,7 +3,7 @@ const amqp = require("amqplib");
 async function recvMail() {
     try {
         const connection = await amqp.connect("amqp://localhost");
-        const channel = await connection.createChannel()
+        const channel = await connection.createChannel();
 
         await channel.assertQueue("subscribed_users_mail_queue", { durable: false });
 
