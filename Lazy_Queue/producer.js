104dc4f708708ced91d56547742ec3e59b7c8f80
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-01 12:47:35 +0530

    commit (Lazy_Queue)

--- Lazy_Queue/producer.js
+++ Lazy_Queue/producer.js
@@ -1,2 +1,10 @@
 const amqp = require("amqplib");
 
+async function setup (message) {
+    const connection  = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+
+    const exchangeName = "notifications_exchange";
+    const queueName = "Lazy_notification_queue";
+    const routingKey = "notification.key";
+}
