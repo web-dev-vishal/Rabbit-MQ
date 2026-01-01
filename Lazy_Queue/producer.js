9de8fcc432301f63475de66f0b122a8302d63f84
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-01 12:57:30 +0530

    commit (Lazy_Queue)

--- Lazy_Queue/producer.js
+++ Lazy_Queue/producer.js
@@ -9,4 +9,24 @@ async function setup(message) {
     const routingKey = "notification.key";
 
     await channel.assertExchange(exchangeName, "direct", { durable: true });
+
+    await channel.assertQueue(queueName, {
+        durable: true,
+        arguments: {
+            "x-queue-node": "lazy"
+        },
+    });
+
+    await channel.bindQueue(queueName, exchangeName, routingKey)
+
+    channel.publish(exchangeName, routingKey, Buffer.from(message), {
+        persistent: true,
+    });
+
+    console.log(`Message sent: ${message}`);
+
+    await channel.close();
+    await connection.close();
 }
+
+setup(`Hello i am a message`)
